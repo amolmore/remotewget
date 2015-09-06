4a2c3e8e2aa14edad60e65754548bfbd3cd01a24
Amolmore <hhgtg42a@gmail.com>
2015-09-06 12:41:07 +0530

    Initial commit

---
+++ RemoteWget/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace RemoteWget
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLocation = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxDisplay = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxCoomand = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxLocation
+            // 
+            this.textBoxLocation.Location = new System.Drawing.Point(72, 115);
+            this.textBoxLocation.Name = "textBoxLocation";
+            this.textBoxLocation.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLocation.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Location";
+            // 
+            // textBoxDisplay
+            // 
+            this.textBoxDisplay.Location = new System.Drawing.Point(16, 139);
+            this.textBoxDisplay.Multiline = true;
+            this.textBoxDisplay.Name = "textBoxDisplay";
+            this.textBoxDisplay.ReadOnly = true;
+            this.textBoxDisplay.Size = new System.Drawing.Size(256, 82);
+            this.textBoxDisplay.TabIndex = 24;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxCoomand
+            // 
+            this.textBoxCoomand.Location = new System.Drawing.Point(72, 89);
+            this.textBoxCoomand.Name = "textBoxCoomand";
+            this.textBoxCoomand.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCoomand.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Url";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(72, 63);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPassword.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Password";
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(72, 37);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUserName.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "User";
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(72, 11);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(100, 20);
+            this.textBoxServer.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Server";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBoxLocation);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxDisplay);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxCoomand);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxServer);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "RemoteWget";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLocation;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxDisplay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxCoomand;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RemoteWget/RemoteWget.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Renci.SshNet;
+
+namespace RemoteWget
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxServer.Text = Properties.Settings.Default.Server;
+            textBoxUserName.Text = Properties.Settings.Default.UserName;
+            textBoxPassword.Text = Properties.Settings.Default.UserPassword;
+            textBoxLocation.Text = Properties.Settings.Default.Location;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string host = textBoxServer.Text.Trim();
+            string userName = textBoxUserName.Text.Trim();
+            string psw = textBoxPassword.Text.Trim();
+            string url = textBoxCoomand.Text.Trim();
+            string location = @" -P  " + textBoxLocation.Text.Trim();
+            string finalCommand = @"wget -bqc '" + url + "' " + location + " ";
+            ConnectionInfo conInfo = new ConnectionInfo(host, 22, userName, new AuthenticationMethod[]{
+                new PasswordAuthenticationMethod(userName,psw)
+            });
+            SshClient client = new SshClient(conInfo);
+            try
+            {
+                client.Connect();
+                var outptu = client.RunCommand(finalCommand);
+            }
+            catch (Exception ex)
+            {
+                textBoxDisplay.Text = ex.Message;
+                throw;
+            }
+
+            client.Disconnect();
+            client.Dispose();
+            SetLastValues(host, userName, psw, textBoxLocation.Text.Trim());
+        }
+
+        private void SetLastValues(string server, string userName, string password, string location)
+        {
+            Properties.Settings.Default.Server = server;
+            Properties.Settings.Default.UserName = userName;
+            Properties.Settings.Default.UserPassword = password;
+            Properties.Settings.Default.Location = location;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
